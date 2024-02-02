6a2d2a1a6340542f7eb0b745ff4b853f9e72c20e
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-02 14:46:37 -0500

    initial commit

---
+++ components/ImageView.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Image } from "react-native";
+
+export default function ImageView({ Placeholderimage, selectedImage }) {
+  const imageSource = selectedImage ? { uri: selectedImage } : Placeholderimage;
+  return <Image source={imageSource} style={styles.image} />;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
